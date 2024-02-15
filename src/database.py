0bad11406307cb26b684c4f148cfd7bf24aba96f
TheHeroBrine422 <caleb@calebgj.io>
2024-02-15 15:19:37 -0600

    add basic connection and allow the program to get users.

--- src/database.py
+++ src/database.py
@@ -1,21 +1,23 @@
 import os
 from supabase import create_client
 
-# Supabase connection
-url = "https://saiciiruhdotdjxznipy.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhaWNpaXJ1aGRvdGRqeHpuaXB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgwMjc4NzEsImV4cCI6MjAyMzYwMzg3MX0.QmYOxHO_M-10pN_eW_Gy4SOpbZ8F0DepmcVTxAzkmy8"
-
-supabase = create_client(url, key)
 
 class DatabaseManager():
     def __init__(self, gameState):
         self.gameState = gameState
-        # add database connection stuff here
+        url = "https://saiciiruhdotdjxznipy.supabase.co"
+        key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhaWNpaXJ1aGRvdGRqeHpuaXB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgwMjc4NzEsImV4cCI6MjAyMzYwMzg3MX0.QmYOxHO_M-10pN_eW_Gy4SOpbZ8F0DepmcVTxAzkmy8"
 
-    # implement database functions for other portions of the code to use.
+        # Supabase connection
+        self.supabase = create_client(url, key)
+
+    def getUsers(self):
+        # Testing whether the connection works
+        response = self.supabase.table('Players').select("*").execute()
+        return response.data
+
     
-    
-    # Testing whether the connection works
-    response = supabase.table('countries').select("*").execute()
-    print(response)
+if __name__ == "__main__":
+    databaseManager = DatabaseManager({})
+    databaseManager.getUsers()
 
