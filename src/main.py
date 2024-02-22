ff0030a5eff5da5903320f27b790fc0ab7a38bd3
Hero <caleb@calebgj.io>
2024-02-22 16:42:46 -0600

    make networking.py nonblocking and fix some bugs with it.

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from networking import NetworkingManager
 from render import RenderingManager
 from database import DatabaseManager
 
-gameState = {"running": True, "stage": "init"} # global stuff like what players on what team, what is the current state of the game (starting, inprogress, finished) and other stuff like that.
+gameState = {"running": True, "stage": "starting"} # global stuff like what players on what team, what is the current state of the game (starting, inprogress, finished) and other stuff like that.
 
 databaseManager = DatabaseManager(gameState)
 networkingManager = NetworkingManager(gameState)
--- src/networking.py
+++ src/networking.py
@@ -15,6 +15,7 @@ class NetworkingManager():
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.s.bind((self.LOCAL_ADDRESS, self.RECV_PORT))
+        self.s.setblocking(False)
         self.gameEndCount = 0
         self.gameState = gameState
 
@@ -22,25 +23,30 @@ class NetworkingManager():
         self.s.sendto(str.encode(msg), (self.BROADCAST_ADDRESS, self.BROADCAST_PORT))
 
 
-
     def tick(self):
         if self.gameState["stage"] == "starting":
             self.send_broadcast("202")
             self.gameEndCount = 0
+            self.gameState["stage"] = "running"
         elif self.gameState["stage"] == "finished" and self.gameEndCount < 3:
             self.send_broadcast("221")
             self.gameEndCount += 1
         else:
-            data, addr = self.s.recvfrom(1024) # check how this function actually returns data, cause it might be a problem depending on how it works.
+            try:
+                data, addr = self.s.recvfrom(1024)
 
-            data = data.split(":")
-            sendingEquipID = data[0]
-            hitEquipID = data[1]
-            if hitEquipID == "53" or hitEquipID == "43":
+                data = data.decode("utf-8")
+
+                data = data.split(":")
+                sendingEquipID = data[0]
+                hitEquipID = data[1]
+                if hitEquipID == "53" or hitEquipID == "43":
+                    pass
+                    # todo: deal with bases
+                elif False and sendingEquipID.team == hitEquipID.team: # todo figure out actually where team data will be located and grab it from there.
+                    self.send_broadcast(sendingEquipID)
+                else:
+                    self.send_broadcast(hitEquipID)
+            except BlockingIOError:
                 pass
-                # todo: deal with bases
-            elif sendingEquipID.team == hitEquipID.team: # todo figure out actually where team data will be located and grab it from there.
-                self.send_broadcast(sendingEquipID)
-            else:
-                self.send_broadcast(hitEquipID)
 
